2c3607fe46a978896c34aecebca15da19751aa5f
Kipras <kipraskb@gmail.com>
2025-04-09 22:58:02 +0300

    feat: design improvments

--- scirpts/index.js
+++ scirpts/index.js
@@ -1,11 +1,11 @@
 var faq = document.querySelectorAll(".faq-item");
 
 faq.forEach((element) => {
-  var button = document.querySelector(".faq-toggle");
-  var content = document.querySelector(".faq-content");
+  var button = element.querySelector(".faq-toggle");
+  var content = element.querySelector(".faq-content");
 
   button.addEventListener("click", () => {
     console.log("siu");
-    content.classList.toggle("faq-content");
+    content.classList.toggle("active");
   });
 });
