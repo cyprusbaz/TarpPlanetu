337229848ce29bbe41fd45c1e506e8300c606f24
Kipras <kipraskb@gmail.com>
2025-05-09 12:33:02 +0300

    created login home page for each user, and its possible to go to them thru the login entering valid information which is found in login&registration.js

--- scirpts/login&registration.js
+++ scirpts/login&registration.js
@@ -1,3 +1,6 @@
+const email = document.querySelector("#login-email");
+const button = document.querySelector(".loginBtn");
+
 document
   .getElementById("show-register")
   .addEventListener("click", function (e) {
@@ -10,4 +13,19 @@ document.getElementById("show-login").addEventListener("click", function (e) {
   e.preventDefault();
   document.getElementById("register-form").classList.add("hide");
   document.getElementById("login-form").classList.remove("hide");
+});
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(email.value);
+  let url = "login&registration.html";
+  if (email.value == "admin@gmail.com") {
+    url = "admin_home_page.html";
+  } else if (email.value == "worker@gmail.com") {
+    url = "worker_home_page.html";
+  } else if (email.value == "user@gmail.com") {
+    url = "user_home_page.html";
+  }
+
+  window.location.href = url;
 });
